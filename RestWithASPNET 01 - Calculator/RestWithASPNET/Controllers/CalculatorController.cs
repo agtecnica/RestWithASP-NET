721564e12bff8dd9a6227df903f18309ecbe9e25
agtecnica <63085268+agtecnica@users.noreply.github.com>
2020-09-28 09:41:55 -0300

    Implementing REST Calculator

--- RestWithASPNET 01 - Calculator/RestWithASPNET/Controllers/CalculatorController.cs
+++ RestWithASPNET 01 - Calculator/RestWithASPNET/Controllers/CalculatorController.cs
@@ -14,8 +14,8 @@ namespace RestWithASPNET.Controllers
     {
 
 
-        // GET api/5/5
-        [HttpGet("{firstnumber}/{secondNumber}")]
+        // GET api/sum/5/5
+        [HttpGet("sum/{firstnumber}/{secondNumber}")]
         public IActionResult Sum(string firstnumber, string secondNumber)
         {
             if (IsNumeric(firstnumber) && IsNumeric(secondNumber))
@@ -25,10 +25,78 @@ namespace RestWithASPNET.Controllers
             }
             return BadRequest("Invalid Input");
         }
+        // GET api/subtraction/5/5
+        [HttpGet("subtraction/{firstnumber}/{secondNumber}")]
+        public IActionResult Sub(string firstnumber, string secondNumber)
+        {
+            if (IsNumeric(firstnumber) && IsNumeric(secondNumber))
+            {
+                var sub = ConvertToDecimal(firstnumber) - ConvertToDecimal(secondNumber);
+                return Ok(sub.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+        // GET api/mult/5/5
+        [HttpGet("mult/{firstnumber}/{secondNumber}")]
+        public IActionResult Mult(string firstnumber, string secondNumber)
+        {
+            if (IsNumeric(firstnumber) && IsNumeric(secondNumber))
+            {
+                var mult = ConvertToDecimal(firstnumber) * ConvertToDecimal(secondNumber);
+                return Ok(mult.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+        // GET api/avg/5/5
+        [HttpGet("avg/{firstnumber}/{secondNumber}")]
+        public IActionResult Avg(string firstnumber, string secondNumber)
+        {
+            if (IsNumeric(firstnumber) && IsNumeric(secondNumber))
+            {
+                var avg = (ConvertToDecimal(firstnumber) + ConvertToDecimal(secondNumber)) / 2;
+                return Ok(avg.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+        // GET api/avg/5/5
+        [HttpGet("sqrt/{firstnumber}")]
+        public IActionResult sqrt(string firstnumber, string secondNumber)
+        {
+            if (IsNumeric(firstnumber))
+            {
+                var sqrt = Math.Sqrt(ConvertToDouble(firstnumber));
+                return Ok(sqrt.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        //soma, divisao, multiplicao, média e raiz quadrada
+        // GET api/Div/3/2
+        [HttpGet("Div/{firstnumber}/{secondNumber}")]
+        public IActionResult Division(string firstnumber, string secondNumber)
+        {
+            if (IsNumeric(firstnumber) && IsNumeric(secondNumber))
+            {
+                if (ConvertToDecimal(secondNumber) == 0)
+                    return BadRequest("Invalid second Number");
+
+                var dividend = ConvertToDecimal(firstnumber) / ConvertToDecimal(secondNumber);
+                return Ok(dividend.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+
 
         private decimal ConvertToDecimal(string number)
         {
             decimal.TryParse(number, out decimal numberConverted);
+            return numberConverted;
+        }
+
+        private double ConvertToDouble(string number)
+        {
+            double.TryParse(number, out double numberConverted);
             return numberConverted;
         }
 
