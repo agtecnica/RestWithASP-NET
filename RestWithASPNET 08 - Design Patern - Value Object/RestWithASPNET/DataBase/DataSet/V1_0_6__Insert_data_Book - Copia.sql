2737e5bc934919a05ab8a49c5803a4d50e7fa6b6
agtecnica <63085268+agtecnica@users.noreply.github.com>
2020-10-01 00:20:49 -0300

    Working in Value Object - VO

---
+++ RestWithASPNET 08 - Design Patern - Value Object/RestWithASPNET/DataBase/DataSet/V1_0_6__Insert_data_Book - Copia.sql
@@ -0,0 +1,2 @@
+INSERT into book (id, author, launchdate, price, title)
+VALUES(NEXTVAL('book_seq'), 'Andre','2020-09-30', 10.00,'Tituloi Livro')
---
+++ RestWithASPNET 08 - Design Patern - Value Object/RestWithASPNET/Repository/Implementattions/PersonRepository.cs
@@ -0,0 +1,100 @@
+﻿using RestWithASPNET.Model;
+using RestWithASPNET.Model.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RestWithASPNET.Repository.Implementattions
+{
+    public class PersonRepository : IPersonRepository
+    {
+        private SqlContext _context;
+
+
+        public PersonRepository(SqlContext context)
+        {
+            _context = context;
+        }
+
+        public Person Create(Person person)
+        {
+            try
+            {
+                _context.Add(person);
+                _context.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return person;
+        }
+
+        public Person Update(Person person)
+        {
+            try
+            {
+                if (!Exists(person.Id)) return null;//verifica se Person existe
+
+                var result = _context.person.SingleOrDefault(p => p.Id.Equals(person.Id));
+                if (result != null)
+                {
+                    _context.Entry(result).CurrentValues.SetValues(person);
+                    _context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return person;
+        }
+
+        public void Delete(long id)
+        {
+            try
+            {
+                var result = _context.person.SingleOrDefault(p => p.Id.Equals(id));
+                if (result != null)
+                {
+                    _context.person.Remove(result);
+                    _context.SaveChanges();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public Person FindById(long id)
+        {
+            try
+            {
+               return  _context.person.SingleOrDefault(p => p.Id.Equals(id));
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public List<Person> FindAll()
+        {
+            try
+            {
+                return _context.person.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public bool Exists(long? id)
+        {
+            return _context.person.Any(p => p.Id.Equals(id));
+        }
+
+    }
+}
