808e432318786730e6819a4080caef2df994df4c
agtecnica <63085268+agtecnica@users.noreply.github.com>
2020-10-01 10:36:48 -0300

    Content Negociation

---
+++ RestWithASPNET 10 - Content Negociation/RestWithASPNET/Business/IBookBusiness.cs
@@ -0,0 +1,15 @@
+﻿using RestWithASPNET.Model;
+using RestWithASPNET.Model.Base;
+using System.Collections.Generic;
+
+namespace RestWithASPNET.Business
+{
+    public interface IBookBusiness
+    {
+        BookVO Create(BookVO item);
+        BookVO FindById(long id);
+        List<BookVO> FindAll();
+        BookVO Update(BookVO item);
+        void Delete(long id);
+    }
+}
---
+++ RestWithASPNET 10 - Content Negociation/RestWithASPNET/Data/VO/BookVO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace RestWithASPNET.Model
+{
+    public class BookVO
+    {
+        [JsonPropertyName("Codigo")]
+        public long? Id { get; set; }
+
+        [JsonPropertyName("Titulo")]
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        [JsonIgnore]
+        public decimal Price { get; set; }
+
+        [JsonPropertyName("Data")]
+        public DateTime LaunchDate { get; set; }
+    }
+}
