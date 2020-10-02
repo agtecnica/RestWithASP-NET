8b39d39e7a4c9aa248298f1eefa7f3fe540c3fab
agtecnica <63085268+agtecnica@users.noreply.github.com>
2020-10-02 00:38:24 -0300

    Authentication without Swagger

---
+++ RestWithASPNET 13 - Authentication without Swagger/RestWithASPNET/Controllers/LoginController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using RestWithASPNET.Business;
+using RestWithASPNET.Model;
+
+namespace RestWithASPNET.Controllers
+{
+    [ApiController]
+    [ApiVersion("1")]
+    [Route("api/v{version:apiversion}/[controller]")]
+    public class LoginController : ControllerBase
+    {
+        private ILoginBusiness _loginBusiness;
+
+        public LoginController(ILoginBusiness loginBusiness)
+        {
+            _loginBusiness = loginBusiness;
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public object Post([FromBody] User user)
+        {
+            if (user == null) return BadRequest();
+
+            return _loginBusiness.FindByLogin(user);
+        }
+    }
+}
---
+++ RestWithASPNET 13 - Authentication without Swagger/RestWithASPNET/Model/Context/SqlContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RestWithASPNET.Model.Context
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext() { }
+
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options) { }
+
+
+        public DbSet<Person> person { get; set; }
+        public DbSet<Book> book { get; set; }
+        public DbSet<User> user { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.HasCollation("my_collation", locale: "en-u-ks-primary", provider: "icu", deterministic: false);
+
+            //builder.UseDefaultColumnCollation("my_collation");
+
+        }
+    }
+}
